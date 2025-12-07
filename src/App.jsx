03fc7e1869b130b6987fa4ce3df5bfc94dbc1e97
Umar Mian <omian3@gmail.com>
2025-12-06 18:03:46 -0600

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
+        <h1>CS 4213 - Shopping Cart Prototype</h1>
+        <p>Patterns: Strategy + Factory Method + Observer</p>
+      </div>
+    </>
+  )
+}
+
+export default App
